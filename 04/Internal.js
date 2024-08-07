88fefe301d1375719663dbbed2f5243ea526a870
Omnia Sherief <102552702+Omniasherief@users.noreply.github.com>
2024-08-07 13:15:58 +0300

    javascript

---
+++ 04/Internal.js
@@ -0,0 +1,5 @@
+console.log(number1); /* 2 it gives undefined as internal is before external file in html*/
+                      /*3 it works when changed order in HTML*/
+                      /*4 it works when you change numbers in console*/
+                      /*5 firstName[3]="A" this will not affect the result */
+console.log("Iam in internal"); 
