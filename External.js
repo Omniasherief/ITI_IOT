68a9a5f60fd044c5ade528045da0a84b7a12999f
Omnia Sherief <102552702+Omniasherief@users.noreply.github.com>
2024-08-07 13:10:28 +0300

    Day4 Javascript

---
+++ External.js
@@ -0,0 +1,13 @@
+console.log(number1);
+
+var number1 = 3;
+var number2 = 2.9;
+var number3 = 0xff;
+
+var firstName = "omnia";
+var middleName = 'sherief';
+var lastName = `ahmed`;
+
+var flag = true;
+
+console.log("This is the External JavaScript file");
