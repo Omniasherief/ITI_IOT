7f2c87b5a931efd768c882b704ea620e94a7559a
Omnia Sherief <102552702+Omniasherief@users.noreply.github.com>
2024-08-08 10:10:28 +0300

    Update External.js

--- 04/External.js
+++ 04/External.js
@@ -1,4 +1,6 @@
-console.log(number1);
+console.log("Before i define");
+console.log(number1); /*if number 1 is defined as var "hosting" it will be undefined 
+but if I define it as let number1=3 it gives error*/
 
 var number1 = 3;
 var number2 = 2.9;
